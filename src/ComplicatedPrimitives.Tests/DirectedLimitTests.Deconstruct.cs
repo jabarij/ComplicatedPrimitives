8e77b12b81c39da799bd8f7c64bb47d3a0f7f4b8
Bartlomiej Soter <bartlomiej.soter@shijigroup.com>
2023-11-15 09:11:08 +0100

    Issue #17 - Developed unit tests for deconstruction of Range and DirectedLimit;

---
+++ src/ComplicatedPrimitives.Tests/DirectedLimitTests.Deconstruct.cs
@@ -0,0 +1,29 @@
+﻿using ComplicatedPrimitives.TestAbstractions;
+using Xunit;
+using AutoFixture;
+using FluentAssertions;
+
+namespace ComplicatedPrimitives.Tests
+{
+    partial class DirectedLimitTests
+    {
+        public class Deconstruct : DirectedLimitTests
+        {
+            public Deconstruct(TestFixture testFixture) : base(testFixture) { }
+
+            [Fact]
+            public void LeftLimitValue_ShouldReturnExpectedResult()
+            {
+                // arrange
+                var sut = Fixture.Create<DirectedLimit<decimal>>();
+
+                // act
+                var (limitValue, side) = sut;
+
+                // assert
+                limitValue.Should().Be(sut.LimitValue);
+                side.Should().Be(sut.Side);
+            }
+        }
+    }
+}
---
+++ src/ComplicatedPrimitives.Tests/RangeTests.Deconstruct.cs
@@ -0,0 +1,28 @@
+﻿using ComplicatedPrimitives.TestAbstractions;
+using FluentAssertions;
+using Xunit;
+
+namespace ComplicatedPrimitives.Tests
+{
+    partial class RangeTests
+    {
+        public class Deconstruct : RangeTests
+        {
+            public Deconstruct(TestFixture testFixture) : base(testFixture) { }
+
+            [Fact]
+            public void LeftLimitValue_ShouldReturnExpectedResult()
+            {
+                // arrange
+                var sut = Fixture.CreateRange<decimal>();
+
+                // act
+                var (left, right) = sut;
+
+                // assert
+                left.Should().Be(sut.Left);
+                right.Should().Be(sut.Right);
+            }
+        }
+    }
+}
