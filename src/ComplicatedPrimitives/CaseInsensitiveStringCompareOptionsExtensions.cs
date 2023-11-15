d63711e32d142094332642fcf81e4559a043c42e
Bartłomiej Soter <soter.dev@gmail.com>
2023-11-15 23:29:26 +0100

    Issue #15 - Implemented basic compare methods for CaseInsensitiveString;

---
+++ src/ComplicatedPrimitives/CaseInsensitiveStringCompareOptionsExtensions.cs
@@ -0,0 +1,32 @@
+﻿using System.Globalization;
+
+namespace ComplicatedPrimitives
+{
+    public static class CaseInsensitiveStringCompareOptionsExtensions
+    {
+        public static CompareOptions ToStringCompareOptions(this CaseInsensitiveStringCompareOptions value)
+        {
+            var result = CompareOptions.IgnoreCase;
+            
+            if (value.HasFlag(CaseInsensitiveStringCompareOptions.IgnoreNonSpace))
+                result |= CompareOptions.IgnoreNonSpace;
+            
+            if (value.HasFlag(CaseInsensitiveStringCompareOptions.IgnoreSymbols))
+                result |= CompareOptions.IgnoreSymbols;
+            
+            if (value.HasFlag(CaseInsensitiveStringCompareOptions.IgnoreKanaType))
+                result |= CompareOptions.IgnoreKanaType;
+            
+            if (value.HasFlag(CaseInsensitiveStringCompareOptions.IgnoreWidth))
+                result |= CompareOptions.IgnoreWidth;
+            
+            if (value.HasFlag(CaseInsensitiveStringCompareOptions.StringSort))
+                result |= CompareOptions.StringSort;
+            
+            if (value.HasFlag(CaseInsensitiveStringCompareOptions.Ordinal))
+                result |= CompareOptions.Ordinal;
+            
+            return result;
+        }
+    }
+}
